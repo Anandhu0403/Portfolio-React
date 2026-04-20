7d586305a178efb80f17c6c78ea8a9f44461b6dd
Anandhu S <anandhu0403@gmail.com>
2026-04-20 21:12:26 +0530

    fix: adjust main layout margin for improved responsiveness

--- Portfolio/src/components/layout/Layout.jsx
+++ Portfolio/src/components/layout/Layout.jsx
@@ -8,7 +8,7 @@ function Layout({children}) {
       <Sidebar/>
       <Navbar/>
 
-      <main className="md:ml-60 xl:ml-1 pt-10 min-h-[calc(100vh-30px)]">
+      <main className="lg:ml-60 xl:ml-1 pt-10 min-h-[calc(100vh-30px)]">
         {children}  
         </main>
     </div>
